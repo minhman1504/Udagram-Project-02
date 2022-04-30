9b6ca8a448f7b353ac6977103dc9f96b98b0286e
Nguyen Minh Man <minhman1504@gmail.com>
2022-04-30 22:02:08 +0700

    complete review error

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response }  from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -13,27 +13,12 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-  app.get("/filteredimage", async (req, res) => {
-    let {image_url} = req.query;
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    
+    const { image_url } : { image_url: string } = req.query;
     //console.log(`image_url: ${image_url}`);
 
-    //    1. validate the image_url query
-
+    //1. validate the image_url query
     //function to validate
     function isURL (image_url:string):boolean {
       var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
@@ -44,34 +29,30 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
         '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
       return !!pattern.test(image_url);
     };
-
-    //console.log(`validURL: ${isURL(image_url)}`);
     if(!isURL(image_url)){
       return res.status(422).send("image_url fail");
     }
 
-    //    2. call filterImageFromURL(image_url) to filter the image
+    //2. call filterImageFromURL(image_url) to filter the image
     try{
-      const photo =  await filterImageFromURL(image_url);
-      //console.log(photo);
-
-      //    3. send the resulting file in the response
+      const photo:string =  await filterImageFromURL(image_url);
+        //3. send the resulting file in the response
       res.status(200).sendFile(photo, () => {
-        //    4. deletes any files on the server on finish of the response
+        //4. deletes any files on the server on finish of the response
         deleteLocalFiles(photo);
       });
     }catch(error)
     {
-      return res.status(422).send(`url is not the image url or get image error`)
+      return res.status(500).send(`url is not the image url or get image error`)
     }
   });
   //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
+  });
   
 
   // Start the Server
